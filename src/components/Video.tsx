7789b550619110593b05bb8b3d7d48255cd56f9e
Daniel Hessel <daniel@sharbe.com.br>
2022-06-26 14:18:27 -0300

    feat: add routing and class player

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,4 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
 import {
   CaretRight,
   DiscordLogo,
@@ -5,39 +6,88 @@ import {
   Lightning,
 } from "phosphor-react";
 
-export function Video() {
+import "@vime/core/themes/default.css";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      description
+      videoId
+      teacher {
+        name
+        bio
+        avatarURL
+      }
+    }
+  }
+`;
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    };
+  };
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug,
+    },
+  });
+
+  if (!data) {
+    return (
+      <div className="flex-1 flex items-center justify-center">
+        <p>Carregando...</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full mx-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full mx-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId={data.lesson.videoId} />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do Ignite Lab
-            </h1>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti
-              error hic, ea dolores quo reiciendis excepturi nemo, voluptates
-              quod aperiam alias cupiditate praesentium sapiente quasi ut. Unde
-              atque error aspernatur.
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/diego3g.png"
+                src={data.lesson.teacher.avatarURL}
                 alt=""
               />
 
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block">
-                  Diego Fernandes
+                  {data.lesson.teacher.name}
                 </strong>
                 <span className="text-gray-200 text-sm block">
-                  CTO at Rocketseat
+                  {data.lesson.teacher.bio}
                 </span>
               </div>
             </div>
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,14 +1,23 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>();
+
+  function Redirect() {
+    window.location.href = "/event/lesson/abertura-do-evento-ignite-lab";
+
+    return null;
+  }
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
 
       <main className="flex flex-1">
-        <Video />
+        {slug ? <Video lessonSlug={slug} /> : <Redirect />}
         <Sidebar />
       </main>
     </div>
